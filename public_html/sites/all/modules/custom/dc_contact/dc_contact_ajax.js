d34a4a7e2dcf15a988fa693040d6c579f5c70ec7
alex <a.domasevich@unlitsolutions.com>
2016-12-01 13:28:11 +0300

    #1007 Add_ajax_to_dc_contact_module
    Add new functional to contact module

---
+++ public_html/sites/all/modules/custom/dc_contact/dc_contact_ajax.js
@@ -0,0 +1,14 @@
+(function ($) {
+    Drupal.ajax.prototype.commands.dc_contact_reset_form = function (ajax, response, status) {
+        jQuery('#dc-contact-form')[0].reset();
+        $('#messages').remove();
+    };
+
+    Drupal.ajax.prototype.commands.dc_contact_add_errors_block = function (ajax, response, status) {
+        $('#messages').remove();
+
+        $('<div id="messages"><div class="section clearfix"><div class="messages error"> <h2 class="element-invisible">Error Message</h2>' + response.errors.price + '</div></div></div>').insertAfter($('#header'));
+        console.log(response.errors.price);
+    };
+
+}(jQuery));
