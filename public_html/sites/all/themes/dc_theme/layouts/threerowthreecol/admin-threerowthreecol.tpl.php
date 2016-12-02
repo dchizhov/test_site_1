7c03d04b0bc9474267c415a5e478cf7467ffe904
alex <a.domasevich@unlitsolutions.com>
2016-12-02 12:59:06 +0300

    #1008  ADD PLUGIN THEME
    Add new plugin theme

---
+++ public_html/sites/all/themes/dc_theme/layouts/threerowthreecol/admin-threerowthreecol.tpl.php
@@ -0,0 +1,16 @@
+<table>
+    <tr>
+        <td colspan="3"><?php print $content['top1']; ?></td>
+    </tr>
+    <tr>
+        <td colspan="3"><?php print $content['top2']; ?></td>
+    </tr>
+    <tr>
+        <td colspan="3"><?php print $content['top3']; ?></td>
+    </tr>
+    <tr>
+        <td><?php print $content['left']; ?></td>
+        <td><?php print $content['right']; ?></td>
+        <td><?php print $content['center']; ?></td>
+    </tr>
+</table>
